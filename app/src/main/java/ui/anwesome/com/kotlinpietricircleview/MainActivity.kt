074107586cf08a03e614c2db3b749600dffc4128
Anwesh43 <anweshthecool0@gmail.com>
2018-04-10 08:29:39 +0530

    adding PieTriCircleView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinpietricircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinpietricircleview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinpietricircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.pietricircleview.PieTriCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PieTriCircleView.create(this)
     }
 }
